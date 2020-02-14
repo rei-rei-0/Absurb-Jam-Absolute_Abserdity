bd4b5bbbcbc1a0abe5d63917ec8be635cb2d3ac1
rei-rei-0 <57510727+rei-rei-0@users.noreply.github.com>
2020-02-14 14:43:19 -0700

    added gameover pplayagin

--- absurbed_platform/Assets/MainMenu.cs
+++ absurbed_platform/Assets/MainMenu.cs
@@ -15,4 +15,10 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quit");
         Application.Quit();
     }
+
+    public void Restart ()
+    {
+        Debug.Log("Restart");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
